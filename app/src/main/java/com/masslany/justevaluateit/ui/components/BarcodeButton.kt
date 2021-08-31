643311c608f448e01e5ece3ec8b53bec5b7a9110
masslany <p.atryk@outlook.com>
2021-08-31 21:45:23 +0200

    Change BarcodeButton.kt background

--- app/src/main/java/com/masslany/justevaluateit/ui/components/BarcodeButton.kt
+++ app/src/main/java/com/masslany/justevaluateit/ui/components/BarcodeButton.kt
@@ -1,37 +1,50 @@
 package com.masslany.justevaluateit.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.masslany.justevaluateit.R
-import com.masslany.justevaluateit.ui.theme.Purple200
+import com.masslany.justevaluateit.ui.theme.RoundedCornerMedium
 
 @Composable
 fun BarcodeButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit
 ) {
-    Button(
-        modifier = modifier,
-        onClick = onClick,
-        shape = RoundedCornerShape(16.dp),
-        colors = ButtonDefaults.buttonColors(backgroundColor = Purple200)
+    val verticalGradientBrush = Brush.verticalGradient(
+        colors = listOf(
+            Color(0xffCD2EFF),
+            Color(0xffA42FF6)
+        )
+    )
+    val shape = RoundedCornerShape(RoundedCornerMedium)
+
+    Box(
+        modifier = modifier
+            .background(
+                brush = verticalGradientBrush,
+                shape = shape
+            )
+            .clip(shape)
+            .clickable { onClick() },
     ) {
-        val icon = if (MaterialTheme.colors.isLight) {
-            painterResource(id = R.drawable.ic_barcode_dark)
-        } else {
-            painterResource(id = R.drawable.ic_barcode_light)
-        }
         Image(
-            painter = icon,
-            contentDescription = stringResource(R.string.content_description_barcode_icon)
+            painter = painterResource(id = R.drawable.ic_barcode_light),
+            contentDescription = stringResource(R.string.content_description_barcode_icon),
+            modifier = Modifier.align(Alignment.Center)
+                .size(32.dp)
         )
     }
 }
