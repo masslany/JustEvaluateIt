2a65beaceed84472520f9de442b5c8702b6dba5d
masslany <p.atryk@outlook.com>
2021-09-02 15:57:39 +0200

    WIP: Add early rating to AddProductScreen.kt

--- app/src/main/java/com/masslany/justevaluateit/presentation/addproduct/AddProductScreen.kt
+++ app/src/main/java/com/masslany/justevaluateit/presentation/addproduct/AddProductScreen.kt
@@ -2,22 +2,28 @@ package com.masslany.justevaluateit.presentation.addproduct
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
 import com.masslany.justevaluateit.R
 import com.masslany.justevaluateit.presentation.components.AppBar
-import com.masslany.justevaluateit.presentation.components.BackNavigationIcon
+import com.masslany.justevaluateit.presentation.components.BarcodeButton
 import com.masslany.justevaluateit.presentation.dashboard.Tile
+import com.masslany.justevaluateit.presentation.ui.theme.BarcodeButtonHeight
 import com.masslany.justevaluateit.presentation.ui.theme.SpaceMedium
-import com.masslany.justevaluateit.presentation.ui.theme.SpaceVeryLarge
 
+@ExperimentalComposeUiApi
 @Composable
 fun AddProductScreen(
     onNavigationIconClicked: () -> Unit
@@ -47,15 +53,54 @@ fun AddProductScreen(
             ) {
 
             }
-            ProductName(
-                modifier = Modifier
-                    .padding(top = SpaceMedium, end = SpaceMedium),
-                value = "",
-                onValueChange = {}
-            )
+            Column {
+                ProductNameField(
+                    modifier = Modifier
+                        .padding(top = SpaceMedium, end = SpaceMedium),
+                    value = "",
+                    onValueChange = {}
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    ProductBarcodeField(
+                        modifier = Modifier
+                            .padding(top = SpaceMedium, end = SpaceMedium)
+                            .weight(1f),
+                        value = "",
+                        onValueChange = {}
+                    )
+                    BarcodeButton(
+                        modifier = Modifier
+                            .padding(top = SpaceMedium)
+                            .height(BarcodeButtonHeight)
+                            .aspectRatio(1f),
+                    ) {
+
+                    }
+                }
+            }
+
         }
 
+        Row {
+            var rating by remember { mutableStateOf(8.0f) }
+            RatingBar(
+                value = rating,
+                onValueChange = {
+                    rating = it
+                },
+                numStars = 10,
+                onRatingChanged = {},
+//                modifier = Modifier.weight(1f, true)
+            )
+            Rating(value = "", onValueChange = {}, modifier = Modifier.weight(1.0f, true))
+        }
+
+
     }
+
+
 }
 
 @Composable
@@ -66,7 +111,7 @@ private fun addPhotoIcon() = if (MaterialTheme.colors.isLight) {
 }
 
 @Composable
-private fun ProductName(
+private fun ProductNameField(
     modifier: Modifier = Modifier,
     value: String,
     onValueChange: (String) -> Unit,
@@ -96,3 +141,75 @@ private fun NamePlaceholder() {
         Text(text = stringResource(R.string.product_name_placeholder))
     }
 }
+
+@Composable
+private fun Rating(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    Column(modifier = modifier) {
+        TextField(
+            value = value,
+            onValueChange = onValueChange,
+//            modifier = Modifier.fillMaxWidth(0.2f),
+            placeholder = { RatingPlaceholder() },
+            colors = TextFieldDefaults.textFieldColors(
+                disabledTextColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            shape = CircleShape,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+    }
+}
+
+@Composable
+private fun RatingPlaceholder() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text = stringResource(R.string.rating_placeholder), overflow = TextOverflow.Ellipsis)
+    }
+}
+
+@Composable
+private fun ProductBarcodeField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    Column(modifier = modifier) {
+        TextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { BarcodePlaceholder() },
+            colors = TextFieldDefaults.textFieldColors(
+                disabledTextColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            shape = CircleShape,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+    }
+}
+
+@Composable
+private fun BarcodePlaceholder() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(text = stringResource(R.string.barcode_placeholder))
+    }
+}
+
+
