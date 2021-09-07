e061e94bfd598b2ed7cc7cea467646e90d1b4b2b
masslany <p.atryk@outlook.com>
2021-09-07 11:43:07 +0200

    Setup basic DI

---
+++ app/src/main/java/com/masslany/justevaluateit/di/DatabaseModule.kt
@@ -0,0 +1,22 @@
+package com.masslany.justevaluateit.di
+
+import android.app.Application
+import androidx.room.Room
+import com.masslany.justevaluateit.data.local.JEIDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application): JEIDatabase {
+        return Room.databaseBuilder(app, JEIDatabase::class.java, "jei_db")
+            .build()
+    }
+}
---
+++ app/src/main/java/com/masslany/justevaluateit/presentation/JEIApplication.kt
@@ -0,0 +1,7 @@
+package com.masslany.justevaluateit.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JEIApplication : Application()
