f1fe0ff0a1676f6b231addac5a469aff08a217a7
Patryk Maślanka <p.atryk@Outlook.com>
2021-09-07 11:47:45 +0200

    Setup Hilt (#12)

--- app/src/main/java/com/masslany/justevaluateit/presentation/MainActivity.kt
+++ app/src/main/java/com/masslany/justevaluateit/presentation/MainActivity.kt
@@ -3,7 +3,11 @@ package com.masslany.justevaluateit.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.masslany.justevaluateit.R
+import com.masslany.justevaluateit.data.local.JEIDatabase
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
