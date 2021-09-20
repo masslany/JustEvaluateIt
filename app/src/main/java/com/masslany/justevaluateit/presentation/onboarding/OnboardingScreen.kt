8f54b3b47a015899482157f29aed58fd46215b7f
masslany <p.atryk@outlook.com>
2021-09-20 23:09:58 +0200

    ktlint

--- app/src/main/java/com/masslany/justevaluateit/presentation/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/masslany/justevaluateit/presentation/onboarding/OnboardingScreen.kt
@@ -121,7 +121,7 @@ fun OnboardingScreen(
         }
 
         val shouldShowNextPageButton = pagerState.currentPage == 0 ||
-                (pagerState.currentPage == 1 && reviewers.isNotEmpty())
+            (pagerState.currentPage == 1 && reviewers.isNotEmpty())
 
         val shouldShowGetStartedButton = pagerState.currentPage == 2 && categories.isNotEmpty()
 
--- app/src/main/java/com/masslany/justevaluateit/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/masslany/justevaluateit/presentation/ui/theme/Color.kt
@@ -5,7 +5,6 @@ package com.masslany.justevaluateit.presentation.ui.theme
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
-
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
