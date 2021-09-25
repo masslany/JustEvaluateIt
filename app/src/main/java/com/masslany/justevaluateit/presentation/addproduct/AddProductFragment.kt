9bf31917b47b1b18dfd571530cf2ed23daa1396d
masslany <p.atryk@outlook.com>
2021-09-25 23:42:03 +0200

    Improve UX

--- app/src/main/java/com/masslany/justevaluateit/presentation/addproduct/AddProductFragment.kt
+++ app/src/main/java/com/masslany/justevaluateit/presentation/addproduct/AddProductFragment.kt
@@ -1,19 +1,15 @@
 package com.masslany.justevaluateit.presentation.addproduct
 
 import android.Manifest
-import android.content.Context
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.ComposeView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -22,7 +18,6 @@ import com.google.zxing.BarcodeFormat
 import com.google.zxing.Result
 import com.kroegerama.kaiteki.bcode.BarcodeResultListener
 import com.kroegerama.kaiteki.bcode.ui.showBarcodeAlertDialog
-import com.masslany.justevaluateit.R
 import com.masslany.justevaluateit.presentation.ui.theme.JustEvaluateItTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
@@ -37,16 +32,17 @@ class AddProductFragment : Fragment(), BarcodeResultListener {
         Manifest.permission.CAMERA
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val requestPermissionLauncher =
-            registerForActivityResult(
-                ActivityResultContracts.RequestPermission()
-            ) {
-
-            }
-        requestPermissionLauncher.launch(requiredCameraPermission)
-    }
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {
+            requireContext().showBarcodeAlertDialog(
+                owner = this@AddProductFragment,
+                listener = this@AddProductFragment,
+                formats = listOf(BarcodeFormat.EAN_8, BarcodeFormat.EAN_13),
+                barcodeInverted = false
+            )
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -76,32 +72,12 @@ class AddProductFragment : Fragment(), BarcodeResultListener {
             viewModel.addProductChannel.collect { event ->
                 when (event) {
                     AddProductEvent.LaunchBarcodeScanner -> {
-                        if (hasCameraPermission(requireContext())) {
-                            requireContext().showBarcodeAlertDialog(
-                                owner = this@AddProductFragment,
-                                listener = this@AddProductFragment,
-                                formats = listOf(BarcodeFormat.EAN_8, BarcodeFormat.EAN_13),
-                                barcodeInverted = false
-                            )
-                        } else {
-                            Toast.makeText(
-                                requireContext(),
-                                getString(R.string.camera_permission_not_granted),
-                                Toast.LENGTH_LONG
-                            ).show()
-                        }
+                        requestPermissionLauncher.launch(requiredCameraPermission)
                     }
                 }
             }
         }
     }
-
-    private fun hasCameraPermission(context: Context) =
-        ContextCompat.checkSelfPermission(
-            context,
-            requiredCameraPermission
-        ) == PackageManager.PERMISSION_GRANTED
-
 
     override fun onBarcodeResult(result: Result): Boolean {
         viewModel.onBarcodeChanged(result.text)
