cd621190872057427fcc820b3d30fc3d03929877
Patryk Maślanka <p.atryk@Outlook.com>
2021-09-25 23:54:46 +0200

    Add barcode scanner to 'add product screen' (#22)
    * Add dependency
    
    * Add barcode scanner support
    
    * Improve UX

--- app/src/main/java/com/masslany/justevaluateit/presentation/addproduct/AddProductFragment.kt
+++ app/src/main/java/com/masslany/justevaluateit/presentation/addproduct/AddProductFragment.kt
@@ -1,24 +1,48 @@
 package com.masslany.justevaluateit.presentation.addproduct
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.Result
+import com.kroegerama.kaiteki.bcode.BarcodeResultListener
+import com.kroegerama.kaiteki.bcode.ui.showBarcodeAlertDialog
 import com.masslany.justevaluateit.presentation.ui.theme.JustEvaluateItTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @ExperimentalComposeUiApi
 @AndroidEntryPoint
-class AddProductFragment : Fragment() {
+class AddProductFragment : Fragment(), BarcodeResultListener {
 
     private val viewModel: AddProductViewModel by viewModels()
+
+    private val requiredCameraPermission by lazy {
+        Manifest.permission.CAMERA
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {
+            requireContext().showBarcodeAlertDialog(
+                owner = this@AddProductFragment,
+                listener = this@AddProductFragment,
+                formats = listOf(BarcodeFormat.EAN_8, BarcodeFormat.EAN_13),
+                barcodeInverted = false
+            )
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,5 +63,28 @@ class AddProductFragment : Fragment() {
                 }
             }
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.addProductChannel.collect { event ->
+                when (event) {
+                    AddProductEvent.LaunchBarcodeScanner -> {
+                        requestPermissionLauncher.launch(requiredCameraPermission)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onBarcodeResult(result: Result): Boolean {
+        viewModel.onBarcodeChanged(result.text)
+        return true
+    }
+
+    override fun onBarcodeScanCancelled() {
+        // Do nothing
     }
 }
